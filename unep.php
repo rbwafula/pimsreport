91e5c82d923dde30f18ce7efde0e33a07c4faef7
Robert Wafula <rbwafula@gmail>
2021-03-03 14:26:17 +0300

    Dashboard update

--- unep.php
+++ unep.php
@@ -52,7 +52,7 @@ include_once 'totals_algo.php';
         <div class="row noprint" data-html2canvas-ignore="true">
             <main role="main" class="col-md-12 ml-sm-auto col-lg-12 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Division Dashboard</h1>
+                    <h1 class="h2">Corporate Dashboard</h1>
                     <div class="btn-toolbar mb-2 mb-md-0">
                         <div class="btn-group mr-2">
                             <button type="button" class="btn btn-sm btn-outline-secondary" onclick="javascript:void(0);">Share</button>
